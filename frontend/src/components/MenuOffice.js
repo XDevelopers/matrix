c0cfe207c79875a7e8dbc0e09504d47dcea96774
Fernando Ultremare <feroult@gmail.com>
2020-05-02 20:34:28 -0300

    spreadsheet rooms

--- frontend/src/components/MenuOffice.js
+++ frontend/src/components/MenuOffice.js
@@ -21,7 +21,7 @@ const useStyles = makeStyles(theme => ({
       backgroundColor: fade(theme.palette.common.white, 0.25)
     },
     marginLeft: 0,
-    marginRight: 20,
+    marginRight: 7,
     width: "100%",
     [theme.breakpoints.up("sm")]: {
       marginLeft: theme.spacing(1),
